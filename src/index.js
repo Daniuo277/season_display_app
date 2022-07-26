25b7216220cc0e7401643f15da94030c4835c947
Daniuo277 <daniel.klim277@gmail.com>
2022-07-26 12:57:54 +0200

    commit_260722

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 
 root.render(<App />);
